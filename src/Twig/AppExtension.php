82cda7190375b6a8a8b508ba9ef4c75c67f333a5
Marek Sterzik <marek.sterzik@gmail.com>
2023-12-17 21:26:29 +0100

    fix

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -2,6 +2,7 @@
 
 namespace App\Twig;
 
+use Symfony\Component\HttpFoundation\RequestStack;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 use Twig\Markup;
@@ -11,8 +12,11 @@ use App\Utility\ResourceLoader;
 
 class AppExtension extends AbstractExtension
 {
-    public function __construct(private EndpointFormatter $endpointFormatter, private ResourceLoader $resourceLoader)
-    {
+    public function __construct(
+        private EndpointFormatter $endpointFormatter,
+        private ResourceLoader $resourceLoader,
+        private RequestStack $requestStack,
+    ) {
     }
 
     public function getFilters(): array
@@ -25,6 +29,7 @@ class AppExtension extends AbstractExtension
         return [
             new TwigFunction('endpoint', [$this, 'formatEndpoint'], ['is_safe' => ['all']]),
             new TwigFunction('load_resource', [$this, 'loadResource']),
+            new TwigFunction('tree_id', [$this, 'getTreeId']),
         ];
     }
 
@@ -36,5 +41,18 @@ class AppExtension extends AbstractExtension
     public function loadResource(string $resource): array
     {
         return $this->resourceLoader->loadResource($resource);
+    }
+
+    public function getTreeId(): ?string
+    {
+        $request = $this->requestStack->getCurrentRequest();
+        if ($request === null) {
+            return null;
+        }
+        $routeParams = $request->attributes->get("_route_params");
+        if (!is_array($routeParams) || !isset($routeParams['tree'])) {
+            return null;
+        }
+        return $routeParams['tree'];
     }
 }
